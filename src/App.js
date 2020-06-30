2d648a811a1aa655e5af96d28e635590afe2e5ff
Michael Martin <mictin3@gmail.com>
2020-06-29 20:04:43 -0600

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -21,16 +21,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
-
-  // componentDidMount() {
-  //   this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-  //     this.setState({ currentUser: user });
-  //   });
-  // }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,14 +5,29 @@ import './form-input.styles.scss';
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
-    {label ? (
+    {label || otherProps.value ? (
       <label
-        className={`${otherProps.value.length} ? 'shrink' : ''} form-input-label`}
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
       >
         {label}
       </label>
     ) : null}
   </div>
 );
+
+// const FormInput = ({ handleChange, label, ...otherProps }) => (
+//   <div className='group'>
+//     <input className='form-input' onChange={handleChange} {...otherProps} />
+//     {label ? (
+//       <label
+//         className={`${otherProps.value.length} ? 'shrink' : ''} form-input-label`}
+//       >
+//         {label}
+//       </label>
+//     ) : null}
+//   </div>
+// );
 
 export default FormInput;
