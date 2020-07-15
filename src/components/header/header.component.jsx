880fd5e2170a3e8519a3df3bee7bfd2e117c0c7d
Michael Martin <mictin3@gmail.com>
2020-07-15 09:51:39 -0600

    Added cart icon to ui, just finished lecture 109

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import { auth } from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component'
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/crown.svg'
 
-import './header.styles.scss';
+import './header.styles.scss'
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -29,12 +30,13 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
-);
+)
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
-});
+})
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)
